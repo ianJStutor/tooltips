de86704df1fdef66da02f81e882d9f4c58ffbd49
Ian Marshall <ian.js.tutor@gmail.com>
2022-08-28 12:29:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tooltips
-Front-end widget
+# Tooltips
+
+## Ian Marshall
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+### Live Site
+
+> [https://ianjstutor.github.io/interactive-background-animations/](https://ianjstutor.github.io/interactive-background-animations/)
+
+### Description
+
+Front-end widget for showing tooltips. Built with vanilla JavaScript, my favorite flavor!
---
+++ tooltips.js
@@ -0,0 +1,41 @@
+var tooltip;
+
+function initTooltips(root = document){
+	root.querySelectorAll("[data-tooltip]").forEach(setupTooltip);
+}
+
+function setupTooltip(el){
+	el.addEventListener("mouseover", tooltipOn);
+	el.addEventListener("pointerover", tooltipOn);
+}
+
+function tooltipOn(e){
+	e.target.removeEventListener("mouseover", tooltipOn);
+	e.target.removeEventListener("pointerover", tooltipOn);
+	e.target.addEventListener("mousemove", tooltipMove);
+	e.target.addEventListener("pointermove", tooltipMove);
+	e.target.addEventListener("mouseout", tooltipOff);
+	e.target.addEventListener("pointerout", tooltipOff);
+	tooltip = document.createElement("div");
+	tooltip.id = "tooltip";
+	tooltip.innerHTML = e.target.getAttribute("data-tooltip");
+	tooltipMove(e);
+	document.body.appendChild(tooltip);
+}
+
+function tooltipMove(e){
+	tooltip.style.left = e.clientX + "px";
+	tooltip.style.top = e.clientY + "px";
+}
+
+function tooltipOff(e){
+	e.target.removeEventListener("mousemove", tooltipMove);
+	e.target.removeEventListener("pointermove", tooltipMove);
+	e.target.addEventListener("mouseover", tooltipOn);
+	e.target.addEventListener("pointerover", tooltipOn);
+	e.target.removeEventListener("mouseout", tooltipOff);
+	e.target.removeEventListener("pointerout", tooltipOff);
+	document.body.removeChild(tooltip);
+}
+
+export { initTooltips };
